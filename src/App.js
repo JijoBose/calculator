f8b5c69402242d5e8fe094ae5f4a28d070e74a5a
Jijo Bose <bosejijo@gmail.com>
2020-11-07 15:19:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Numbers from '../src/components/Numbers';
+
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+
+const buttons = [
+  ['CLEAR', 'DEL'],
+  ['7', '8', '9', '÷'],
+  ['4', '5', '6', 'x'],
+  ['1', '2', '3', '+'],
+  ['.', '0', '=', '-'],
+];
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          <View style={styles.body}>
+            <Numbers buttons={buttons} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ src/components/Numbers/styles.js
@@ -0,0 +1,25 @@
+export default {
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  txtDefault: {
+    color: '#000',
+    fontFamily: 'Helvetica-Light',
+    fontSize: 20,
+  },
+
+  contRow: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+
+  contButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 0.5,
+    height: 80, /// To-do
+    borderColor: '#ecf0f1',
+  },
+};
